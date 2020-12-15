f701c69f4d2efd85d57baa4a30cf6b127bc4eb85
Anderson de Brito Oliveira <andersondebritto@gmail.com>
2020-12-15 12:08:18 +0000

    Update repackage skip

--- pet-clinic-data/src/main/java/com/abo/petclinic/Main.java
+++
@@ -1,9 +0,0 @@
-package com.abo.petclinic;
-
-/**
- * This class was created to avoid 'repackage failed: Unable to find main class'
- */
-public class Main {
-
-    public static void main(String[] args){}
-}
