0fd19ab98d08bda6c810304bb8fca5b852c3cfd3
Anderson de Brito Oliveira <andersondebritto@gmail.com>
2020-12-15 12:00:01 +0000

    Refactoring to multmodule maven project. closes #2.

---
+++ pet-clinic-data/src/main/java/com/abo/petclinic/Main.java
@@ -0,0 +1,9 @@
+package com.abo.petclinic;
+
+/**
+ * This class was created to avoid 'repackage failed: Unable to find main class'
+ */
+public class Main {
+
+    public static void main(String[] args){}
+}
