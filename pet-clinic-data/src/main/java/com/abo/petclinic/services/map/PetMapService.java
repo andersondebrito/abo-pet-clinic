00c513b87c9cc0057cd1b0339f19ab7afd01ec3a
Anderson de Brito Oliveira <andersondebritto@gmail.com>
2020-12-24 18:31:03 +0000

    Update Profiles to use Spring Data JPA Services Closes #47

--- pet-clinic-data/src/main/java/com/abo/petclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/abo/petclinic/services/map/PetMapService.java
@@ -2,11 +2,13 @@ package com.abo.petclinic.services.map;
 
 import com.abo.petclinic.model.Pet;
 import com.abo.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class PetMapService extends AbstractMapService<Pet,Long> implements PetService {
 
     @Override
--- pet-clinic-data/src/main/java/com/abo/petclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/com/abo/petclinic/services/map/SpecialtyMapService.java
@@ -2,11 +2,13 @@ package com.abo.petclinic.services.map;
 
 import com.abo.petclinic.model.Specialty;
 import com.abo.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class SpecialtyMapService extends AbstractMapService<Specialty,Long> implements SpecialtyService {
 
     @Override
