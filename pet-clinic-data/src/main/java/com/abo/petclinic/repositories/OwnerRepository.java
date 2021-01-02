71b5a61d0177b8b623fdee10712559f03674937f
Anderson de Brito Oliveira <andersondebritto@gmail.com>
2021-01-02 19:53:18 +0000

    Implement Find Owner Closes #53

--- pet-clinic-data/src/main/java/com/abo/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/abo/petclinic/repositories/OwnerRepository.java
@@ -3,8 +3,11 @@ package com.abo.petclinic.repositories;
 import com.abo.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
