ef8a01f27e3b2aad83f3f1955c32d69f9b515f7e
Anderson de Brito Oliveira <andersondebritto@gmail.com>
2021-01-09 16:11:57 +0000

    Implements Vet List View. closes #68

--- pet-clinic-web/src/main/java/com/abo/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/abo/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.abo.petclinic.controllers;
 
+import com.abo.petclinic.model.Vet;
 import com.abo.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -14,11 +19,12 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(Model model) {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
+
 }
